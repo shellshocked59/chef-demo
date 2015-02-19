2acf7235ac73885147ace8342fb940730660d4d8
Chef training@getchef.com <chef@ip-172-31-32-65.ec2.internal>
2015-02-19 22:23:14 +0000

    created a template

--- motd/recipes/default.rb
+++ motd/recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+file "/etc/motd" do
+	action :create
+	#content IO.read("motd.txt")
+	content "motd.erb"
+	mode "0644"
+	owner "root"
+	group "root"
+end
