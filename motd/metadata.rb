e1a9e4fc69260a5dac03c877d3c949273b4bb543
Chef training@getchef.com <chef@ip-172-31-32-65.ec2.internal>
2015-02-19 20:18:10 +0000

    adding motd cookbook

---
+++ motd/metadata.rb
@@ -0,0 +1,8 @@
+name             'motd'
+maintainer       'The Authors'
+maintainer_email 'eric_washburn@the1stmovement.com'
+license          'all_rights'
+description      'Installs/Configures motd'
+long_description 'Installs/Configures motd'
+version          '0.1.0'
+
