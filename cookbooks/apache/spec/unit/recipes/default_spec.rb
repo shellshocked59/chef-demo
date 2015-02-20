923a8a3e7b359bdf6efa93f9d101a582760e6433
Chef training@getchef.com <chef@ip-172-31-32-65.ec2.internal>
2015-02-20 00:50:20 +0000

    update

--- cookbooks/apache/spec/unit/recipes/default_spec.rb
+++ cookbooks/apache/spec/unit/recipes/default_spec.rb
@@ -4,20 +4,15 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-require 'spec_helper'
+require 'chefspec'
 
 describe 'apache::default' do
 
-  context 'When all attributes are default, on an unspecified platform' do
+  let(:chef_run) do
+    ChefSpec::Runner.new.converge(described_recipe)
+  end
 
-    let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      chef_run # This should not raise an error
-    end
-
+  it 'installs apache' do
+    expect(chef_run).to install_package('httpd');
   end
 end
